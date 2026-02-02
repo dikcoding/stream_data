aba48486c7d4af97b54273ee6fa97025b47c5438
dikcoding <andikafadillasiagian0@gmail.com>
2026-02-03 06:31:32 +0700

    feat: etl dags

--- airflow/dags/load_songs_dag.py
+++ airflow/dags/load_songs_dag.py
@@ -18,7 +18,7 @@ default_args ={
 
 AIRFLOW_HOME = os.environ.get('AIRFLOW_HOME', '/opt/airflow')
 
-URL = 'https://github.com/ankurchavda/streamify/raw/main/dbt/seeds/songs.csv'
+URL = 'https://github.com/dikcoding/stream_data/tree/main/streamify_dbt/seeds/songs.csv'
 CSV_FILENAME = 'songs.csv'
 PARQUET_FILENAME = CSV_FILENAME.replace('csv', 'parquet')
 
