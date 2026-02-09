932b06eb39b8534a35a4ce476f6a1be5c308a546
dikcoding <andikafadillasiagian0@gmail.com>
2026-02-09 15:33:11 +0700

    feat: fix bug spark

--- spark_stream/streaming_functions.py
+++ spark_stream/streaming_functions.py
@@ -29,7 +29,6 @@ def create_or_get_spark_session(app_name, master="yarn"):
     spark = (SparkSession
              .builder
              .appName(app_name)
-             .master(master=master)
              .getOrCreate())
 
     return spark
